5e95bffc5791da4e8f8cdbb368da680b508380f1
오태현 <jeffreyoh121@gmail.com>
2024-01-25 13:55:08 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.01 ms, Memory: 71.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[n];
+        
+        for (int i=0; i<num_list.length; i++) {
+            if (i < n) {
+                answer[i] = num_list[i];
+                continue;
+            }
+            
+            break;
+        }
+        
+        return answer;
+    }
+}
